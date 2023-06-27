eb8100ef2d805f1dcedb48da3de3d4dc100b1ab8
dasesu <98413431+dasesu@users.noreply.github.com>
2023-06-28 00:14:09 +0200

    Update sudoku_solver.cpp

--- sudoku_solver.cpp
+++ sudoku_solver.cpp
@@ -5,66 +5,81 @@ using namespace std;
 
 
 int sudoku[9][9]{};
-char mapa[9][9]{};
 
 
-int verify_number(int j, int i, int number){
+bool is_valid(int j, int i, int number){
 // retorna 1 si es una posicion valida para ese numero
-
-   int valid = 1;
 
    // Verificamos en la fila
    for (int ii = 0; ii < 9; ++ii){
       if( sudoku[j][ii] == number ){
-         valid=0;
          //printf("Invalido: %d ya se encuentra  en la misma fila\n", number);
-         return valid;
+         return false;
       }
    }
 
    // Verificamos en la columna
    for (int jj = 0; jj < 9; ++jj){
       if( sudoku[jj][i] == number ){
-         valid=0;
          //printf("Invalido: %d ya se encuentra  en la misma columna\n", number);
-         return valid;
+         return false;
       }
    }
 
    // Verificamos en el cuadro 3x3 que contiene las coordenadas
-   for (int jj = j/3; jj < 9; ++jj){
-      for (int ii = i/3; ii < 9; ++ii){
-         if( sudoku[j][ii] == number ){
-            valid=0;
+   int a = j - j%3;
+   int b = i - i%3;
+   for (int jj = a; jj < a + 3 ; jj++){
+      for (int ii = b; ii < b + 3; ii++){
+         if( sudoku[jj][ii] == number ){
             //printf("Invalido: %d ya se encuentra  en el cuadro 3x3\n", number);
-            return valid;
+            return false;
          }
       }
    }
-   
-   return valid;
+   return true;
 }
 
-int es_solucion(){
-   int valid=1;
-   for (int jj = 0; jj < 9; ++jj){
-      for (int ii = 0; ii < 9; ++ii){
-         if( sudoku[jj][jj] != 0 && verify_number(jj, ii, sudoku[jj][jj] ) != 0 ){
-            valid=0;
-            return valid;
-         }
-      }
-   }
-   return valid;
+
+
+bool solveSudoku(int jj, int ii){
+
+    if (jj == 9 - 1 && ii == 9){
+        return true;
+    }
+
+    if (ii == 9) {
+        jj++;
+        ii = 0;
+    }
+
+    // si esta posicion ya esta asignada la salto.
+    if (sudoku[jj][ii] > 0){
+        return solveSudoku(jj, ii+1);
+    } 
+    // los valores posibles son:
+    // 1, 2, 3, 4, 5, 6, 7, 8, 9
+    for (int num = 1; num <= 9; num++){
+        if ( is_valid( jj, ii, num) ){
+            sudoku[jj][ii] = num;
+            if (solveSudoku( jj, ii + 1)){
+                return true;
+            }
+        }
+        sudoku[jj][ii] = 0;
+    }
+    return false;
 }
+
+
+
+
+
 
 
 int main(){
    FILE* fp;
    
-   int num{};
-
-
    fp = fopen("sudoku.txt","r");
    if (fp == NULL) {
       printf("No such file or directory");
@@ -75,14 +90,8 @@ int main(){
       for (int jj = 0; jj < 9; ++jj){
          for (int ii = 0; ii < 9; ++ii){
             fscanf(fp, "%d", &sudoku[jj][ii]);
-            if(sudoku[jj][ii] == 0){
-               mapa[jj][ii] = '.';
-            }else{
-               mapa[jj][ii] = '#';
-            }
          }
       }
-      
       
       printf("Sudoku to solve:\n");
       for (int jj = 0; jj < 9; ++jj){
@@ -95,21 +104,18 @@ int main(){
          printf("\n");
       }
 
-
-      printf("\n");
-      printf("Current map:\n");
-      for (int jj = 0; jj < 9; ++jj){
-         for (int ii = 0; ii < 9; ++ii){
-            printf("%c", mapa[jj][ii]);
-            if( ii<8 ){
-               printf(" ");
+      if( solveSudoku(0,0)  ){
+         printf("Solucion: \n");
+         for (int jj = 0; jj < 9; ++jj){
+            for (int ii = 0; ii < 9; ++ii){
+               printf("%d", sudoku[jj][ii]);
+               if( ii<8 ){
+                  printf(" ");
+               }
             }
+            printf("\n");
          }
-         printf("\n");
       }
-
-      
-
    }
    return 0;
 }
